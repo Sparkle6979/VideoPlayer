fbc95c2da1d0fca700c5e4b0c2b5849e5bba0c93
sparkle6979 <liuhl79@163.com>
2023-10-25 19:54:27 +0800

    create project

---
+++ videoplayer/src/main/java/org/zjudevelop/videoplayer/VideoplayerApplication.java
@@ -0,0 +1,13 @@
+package org.zjudevelop.videoplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoplayerApplication.class, args);
+	}
+
+}

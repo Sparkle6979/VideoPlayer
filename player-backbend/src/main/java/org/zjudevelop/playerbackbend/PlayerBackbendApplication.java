002b53d777632687fba32862090f406275bf6364
sparkle6979 <liuhl79@163.com>
2023-10-25 21:09:28 +0800

    create project

---
+++ player-backbend/src/main/java/org/zjudevelop/playerbackbend/PlayerBackbendApplication.java
@@ -0,0 +1,13 @@
+package org.zjudevelop.playerbackbend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerBackbendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayerBackbendApplication.class, args);
+    }
+
+}
